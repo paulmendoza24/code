06cfc3acdabab0554080201736d8ed26e9a3a9c7
Paul Mendoza <falconwizard01@gmail.com>
2025-08-22 23:40:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,20 @@ def main():
     import pyfiglet
     def add_definition(term, meaning, usage):
         return {
-            "term": term,
-            "meaning": meaning,
-            "usage": usage
-        }
-    var = pyfiglet.figlet_format("Definition Stored")
+            term: meaning,
+            'usage': usage
+            }
+    title = pyfiglet.figlet_format("Definition\nStored", font="cybermedium")
     term = input("Enter a term: ")
-    meaning = input(f"Enter the Meaning of {term}: ")
+    meaning = input(f"Enter the definition of {term}: ")
     usage = input(f"Enter the usage of {term}: ")
-
     glossary = add_definition(term, meaning, usage)
 
-    print("-"*60)
-    print("\nMendoza, Niño Paul C.")
-    print(var)
-    print(f"Term: {glossary['term']}")
-    print(f"Meaning: {glossary['meaning']}")
-    print(f"Usage: {glossary['usage']}")
-    print("-"*60)
-
+    print("="*(len(usage)+10))
+    print(title)
+    print(f"[*] {term}: {glossary[term]}")
+    print(f"[*] Usage: {glossary['usage']}")
+    print("="*(len(usage)+10))
 
 if __name__ == "__main__":
     main()
